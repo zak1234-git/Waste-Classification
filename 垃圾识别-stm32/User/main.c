927bf0429d9b98e2578cb1369d991d58c30c477f
zak1234-git <qwe25011407260809@163.com>
2024-08-29 14:38:54 +0800

    Update main.c

--- 垃圾识别-stm32/User/main.c
+++ 垃圾识别-stm32/User/main.c
@@ -1,12 +1,7 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-#include "OLED.h"
 #include "Servo.h"
-#include "Key.h"
 #include "Serial.h"
-
-uint8_t KeyNum;
-float Angle;
 
 int main(void)
 {
